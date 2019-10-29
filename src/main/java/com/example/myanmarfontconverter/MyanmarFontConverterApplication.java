f6b0af98ab8b3f03bef8c7ca00f7cf645ad68e61
Phyo Pyae <phyopyae2@gmail.com>
2019-10-29 16:37:47 +0800

    Change cross origin address and removed unused result page

--- src/main/java/com/example/myanmarfontconverter/MyanmarFontConverterApplication.java
+++ src/main/java/com/example/myanmarfontconverter/MyanmarFontConverterApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:8096")
+@CrossOrigin(origins = "http://localhost:8080")
 @SpringBootApplication
 public class MyanmarFontConverterApplication {
 
